1f0d5e0e0b8a6f99b13f42ac7cb3033145b8d4cb
nickpaolino <nick_paolino@yahoo.com>
2021-04-30 21:53:29 -0400

    starting to get movement

--- app.js
+++ app.js
@@ -6,19 +6,24 @@ let ballX = 50;
 window.onload = function () {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    drawEverything()
-    drawEverything()
-    drawEverything()
-    
+    let framesPerSecond = 30;
+    setInterval(() => {
+        moveEverything ();
+        drawEverything();
+    }, 1000/framesPerSecond);  
+}
+
+funciton moveEverything () {
+    ballX = ballX + 5;
 }
 
 function drawEverything() {
-    ballX = ballX + 10;
-    console.log(ballX)
+  
+    
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0,0,canvas.width, canvas.height);
     canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(225, 210, 200, 200);
+    canvasContext.fillRect(0, 210, 10, 100);
     canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(ballX, 200, 50, 25);
+    canvasContext.fillRect(ballX, 100, 10, 10);
 }
